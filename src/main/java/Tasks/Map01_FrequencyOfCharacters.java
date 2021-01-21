8cc0bc2fec58a59a82aa4241f47dbaa0e9f065be
akyuzf <akyomr.us@gmail.com>
2021-01-20 20:18:06 -0500

    "commit"

--- src/main/java/Tasks/Map01_FrequencyOfCharacters.java
+++ src/main/java/Tasks/Map01_FrequencyOfCharacters.java
@@ -1,4 +1,9 @@
 package Tasks;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /*
 Write a method that prints the frequency of each character from a String
  */
@@ -7,37 +12,34 @@ public class Map01_FrequencyOfCharacters {
     public static void main(String[] args) {
 
         String str = "picture perfect";
-        int[] freq = new int[str.length()];
-        int i, j;
-
-        //Converts given string into character array
-        char string[] = str.toCharArray();
         System.out.println(str);
-        System.out.println(string);
 
-        for (i = 0; i < str.length(); i++) {
-            freq[i] = 1;
-            for (j = i + 1; j < str.length(); j++) {
-                if (string[i] == string[j]) {
-                    freq[i]++;
+        String str2 = str.replaceAll("\\s", "");
+        System.out.println(str2);
 
-                    //Set string[j] to 0 to avoid printing visited character
-                    string[j] = '0';
-                }
+
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (Character each : str.toCharArray()) {
+
+            if (map.containsKey(each)) {
+
+                map.put(each, map.get(each) + 1);
+
+            } else {
+
+                map.put(each, 1);
 
             }
 
         }
 
-
-        //Displays the each character and their corresponding frequency
-        System.out.println("Characters and their corresponding frequencies");
-        for(i = 0; i <freq.length; i++) {
-            if(string[i] != ' ' && string[i] != '0')
-                System.out.println(string[i] + "-" + freq[i]);
-        }
-
+        System.out.println(map);
 
 
     }
+
+
 }
+
