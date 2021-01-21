c368251aab431c2e08a8807df09b55be83291bf8
akyuzf <akyomr.us@gmail.com>
2021-01-21 18:22:40 -0500

    commit

---
+++ src/main/java/Tasks/Numbers01_ArmstrongNumbers.java
@@ -0,0 +1,40 @@
+package Tasks;
+
+import java.util.Scanner;
+
+/*
+Write a method that can check if a number is Armstrong  number
+Armstrong number is a number that is equal to the sum of cubes of its digits. 
+For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+ */
+public class Numbers01_ArmstrongNumbers {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scan.nextInt();
+
+
+        int rem, div, total = 0;
+
+        div = number; // number=153
+
+        // 153    ->    15      ->          1
+        while (div != 0)
+        {
+            rem = div % 10;              // 3         // 5                // 1
+            div /= 10;                   // 15        // 1                // 0
+            total = total + rem*rem*rem; // 3*3*3=27  // 27+(5*5*5)=152   // 152+(1*1*1)=153
+        }
+
+        if(total == number)
+            System.out.println(number + " is an Armstrong number");
+        else
+            System.out.println(number + " is not an Armstrong number");
+
+
+
+    }
+
+}
---
+++ src/main/java/Tasks/Numbers02_DivideWithoutDivisionOperator.java
@@ -0,0 +1,35 @@
+package Tasks;
+
+public class Numbers02_DivideWithoutDivisionOperator {
+
+    public static void main(String[] args) {
+
+
+        int num1 = 100;
+        int num2 = 11;
+
+            if(num2==0) {
+
+                System.out.println("Invalid Number");
+
+                return;
+
+            }
+
+            System.out.print(num1 +" divided by "+num2 +" is ");
+
+            int count =0;
+
+            while(num1 >= num2) {
+
+                num1 -= num2;
+
+                count++;
+
+            }
+
+            System.out.println(count+", and remainder is "+num1);
+
+        }
+
+    }
