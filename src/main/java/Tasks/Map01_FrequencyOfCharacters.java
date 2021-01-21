48d513ffec2dcb71977e52566f9d0fac205709b9
akyuzf <akyomr.us@gmail.com>
2021-01-20 19:25:39 -0500

    commit

--- src/main/java/Tasks/Map01_FrequencyOfCharacters.java
+++ src/main/java/Tasks/Map01_FrequencyOfCharacters.java
@@ -1,5 +1,12 @@
 package Tasks;
+/*
+Write a method that prints the frequency of each character from a String
+ */
+public class Map01_FrequencyOfCharacters{
 
-public class Map01_FrequencyOfCharacters
-{
+    public static void main(String[] args) {
+
+
+    }
+
 }
